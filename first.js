5f371270f948b5d9649380367515a195ee401626
John Elesho <oluwadamilareelesho@gmail.com>
2023-05-19 16:31:35 +0100

    Basic Introduction of the javascript Language

--- first.js
+++ first.js
@@ -1,1 +1,34 @@
-console.log("This is our first js");
+// -- Language contruct
+// -- variables and data types
+// -- control structures
+// -- Functions
+
+// document.body.style.backgroundColor = "blue"
+// document.body.querySelector("#btn").onclick = ()=>{
+// document.body.querySelector("h1").className = "js"
+
+// }
+
+// DRY
+// YAGNI
+
+// function
+const redirectUser = ()=>{
+    alert("Please wait while we redirect you...")
+    location.href = "second.html"
+}
+// alert('hello world')
+let age = prompt("How old are you")
+alert(`Wow you are ${age} years old`)
+
+if (age < 18)
+{
+    redirectUser()
+}
+else if(age > 60){
+    redirectUser()
+}
+
+
+
+// datatypes
